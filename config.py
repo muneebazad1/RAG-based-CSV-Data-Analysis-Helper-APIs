13a929dac59de59db989234e03542cf5b0a4a63c
Muneeb Azad <muneebazad454@gmail.com>
2025-11-07 17:44:17 +0500

    Add user instruction for API key in config.py
    Added user instruction for API key setup.

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 def setup_environment():
     os.environ["LANGSMITH_TRACING"] = "true"
-    os.environ["LANGSMITH_API_KEY"] = ""
+    os.environ["LANGSMITH_API_KEY"] = ""   #User your own keys
     os.environ["GOOGLE_API_KEY"] = ""
+
     print("✅ Environment variables set.")
